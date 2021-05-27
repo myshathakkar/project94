7f01e1a6ee2defa757f7c3db6af9fb72363d0ae3
myshathakkar <78802180+myshathakkar@users.noreply.github.com>
2021-05-27 17:38:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function login1(){
+    username=document.getElementById("username").value  
+    localStorage.setItem("user name" , username) 
+    window.location("miny_book.html")
+}
---
+++ minibook.js
@@ -0,0 +1,13 @@
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCwOUZrO_2aZ-hAxi63CSPq9Zs0oTgye3U",
+    authDomain: "minibook-62cd6.firebaseapp.com",
+    projectId: "minibook-62cd6",
+    storageBucket: "minibook-62cd6.appspot.com",
+    messagingSenderId: "670872447097",
+    appId: "1:670872447097:web:a60deab422b6cd43998ca8",
+    measurementId: "G-V4J6VV7VZ2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
